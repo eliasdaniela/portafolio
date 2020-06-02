7f44c646a758bd5ef74b9e0df984627e6d83368b
eliasdaniela <42419328+eliasdaniela@users.noreply.github.com>
2020-06-02 11:16:12 -0500

    2 jun

--- js/main.js
+++ js/main.js
@@ -7,3 +7,11 @@ menu.addEventListener("click", menuToggler);
 function menuToggler() {
   menuItems.toggleAttribute("hidden");
 }
+
+/* Proyectos */
+const proyectos = document.querySelectorAll(".proyectos-list li");
+
+proyectos.forEach(function(el) {
+  el.addEventListener("mouseenter", i => i.target.classList.add("imagen"));
+  el.addEventListener("mouseleave", i => i.target.classList.remove("imagen"));
+});
